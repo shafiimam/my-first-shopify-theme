c3a6e60588ee58fdeb02626f1f32380a11a10d0e
shafiimam <shafi15-8072@diu.edu.bd>
2021-09-16 17:05:37 +0600

    second day

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,6 @@
 // Put your application javascript here
+document.querySelector('#sort_by').addEventListener('change', function(e){
+    let url = new URL(window.location.href);
+    url.searchParams.set('sort_by', e.currentTarget.value);
+    window.location.href = url.href;
+})
